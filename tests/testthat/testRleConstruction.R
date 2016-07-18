52f9677b9550b07a0795c47fc7b410a3be4fe9d1
JohnReid <johnbaronreid@netscape.net>
2016-07-18 20:11:36 +0100

    Remove messages.

--- tests/testthat/testRleConstruction.R
+++ tests/testthat/testRleConstruction.R
@@ -2,12 +2,14 @@ library(Saturn)
 context("Rle construction")
 
 test_that("both methods of Rle construction produce the same answers", {
-  for (L in seq(10, 10, step=10)) {
-    .idxs <- sample(L, sample(L, 1))
-    Rle.slow <- sparse.to.rle(L, .idxs, 1:length(.idxs))
+  # For different lengths
+  for (L in seq(1, 1000, by=5)) {
+    # Sample a random number of random indexes
+    .idxs <- sort(sample(L, sample(L, 1)))
+    suppressWarnings(Rle.slow <- sparse.to.rle(L, .idxs, 1:length(.idxs)))
+    # print(Rle.slow)
     Rle.fast <- Rle.from.sparse(L, .idxs, 1:length(.idxs))
-    message(Rle.slow)
-    message(Rle.fast)
+    # print(Rle.fast)
     expect_equal(Rle.slow, Rle.fast)
   }
 })
