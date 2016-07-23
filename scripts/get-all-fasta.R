0c169594e5993dd591e655d4396894a460f9e965
JohnReid <johnbaronreid@netscape.net>
2016-07-23 09:28:24 +0100

    Change to HTML output.

--- scripts/get-all-fasta.R
+++ scripts/get-all-fasta.R
@@ -3,6 +3,6 @@ devtools::load_all()
 for (TF in levels(tfs$TF)) {
     rmarkdown::render(
         "get-TF-fasta.Rmd",
-        output_file = str_c('FASTA-', TF, '.pdf'),
+        output_file = str_c('FASTA-', TF, '.html'),
         params = list(TF = TF))
 }
