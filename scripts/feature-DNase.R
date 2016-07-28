aa661a741b980955cb383af32d2181ce66dbdb19
JohnReid <johnbaronreid@netscape.net>
2016-07-28 18:56:24 +0100

    Update DNase feature script.

---
+++ scripts/feature-DNase.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+#
+# Make DNase summary features
+#
+
+devtools::load_all()
+library(Saturn)
+
+for (cell in levels(cells$cell)) {
+  message(cell)
+  # Load data
+  dnase.summary  <- summarise.dnase(cell, type = 'relaxed')
+  message(cell, ' DNase summary: ', object.size(dnase.summary), ' bytes')
+  # Save data
+  feature.file <- file.path(saturn.data(), 'Features', 'DNase', str_c('dnase-summary-', cell, '.rds'))
+  saveRDS(dnase.summary, feature.file)
+}
