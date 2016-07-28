087934376276e3e17f49b15a0e70617e54342768
JohnReid <johnbaronreid@netscape.net>
2016-07-28 19:03:59 +0100

    Update script.

--- scripts/feature-DNase.R
+++ scripts/feature-DNase.R
@@ -7,11 +7,13 @@ devtools::load_all()
 library(Saturn)
 
 for (cell in levels(cells$cell)) {
-  message(cell)
-  # Load data
-  dnase.summary  <- summarise.dnase(cell, type = 'relaxed')
-  message(cell, ' DNase summary: ', object.size(dnase.summary), ' bytes')
-  # Save data
-  feature.file <- file.path(saturn.data(), 'Features', 'DNase', str_c('dnase-summary-', cell, '.rds'))
-  saveRDS(dnase.summary, feature.file)
+  if ("" != cell) {
+    message(cell)
+    # Load data
+    system.time(dnase.summary  <- summarise.dnase(cell, type = 'relaxed'))
+    message(cell, ' DNase summary: ', object.size(dnase.summary), ' bytes')
+    # Save data
+    feature.file <- file.path(saturn.data(), 'Features', 'DNase', str_c('dnase-summary-', cell, '.rds'))
+    saveRDS(dnase.summary, feature.file)
+  }
 }
