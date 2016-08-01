ac76031965ad4b35364b050296c184fe89887105
JohnReid <johnbaronreid@netscape.net>
2016-08-01 20:43:19 +0100

    Tidy up get FASTA scripts.

--- scripts/get-all-fasta.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-TF <- commandArgs()[2]
-devtools::load_all()
-rmarkdown::render(
-    "get-TF-fasta.Rmd",
-    output_file = str_c('FASTA-', TF, '.html'),
-    params = list(TF = TF))
