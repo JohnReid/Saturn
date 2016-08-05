a205532bc72f021323efb66c7aa311963ed53c48
JohnReid <johnbaronreid@netscape.net>
2016-08-05 11:00:44 +0100

    Add analysis of binding correlation between TFs.

--- scripts/binding-correlations.R
+++ scripts/binding-correlations.R
@@ -37,7 +37,8 @@ pearson.cor <- function(x, y) {
 system.time(pearson.cor(x.rle, y.rle))
 cor(x, y)
 
-#' Analyse each TF
+
+#' Analyse each TF for correlations between binding across cells
 #'
 analyse.tf <- function(tf.meta) {
   tf <- tf.meta$TF[1]
@@ -69,9 +70,8 @@ analyse.tf <- function(tf.meta) {
   data.frame(cell.1 = cell.pairs[1,], cell.2 = cell.pairs[2,], cor = cors)
 }
 cor.by.tf <- tfs %>% filter('train' == split) %>% group_by(TF) %>% do(analyse.tf(.)) %>% ungroup()
-sapply(cor.by.tf, class)
 cor.by.tf
-devtools::use_data(cor.by.tf)
+# devtools::use_data(cor.by.tf)
 
 
 #
@@ -82,6 +82,39 @@ ggplot(cor.by.tf, aes(x = cell.1, y = cell.2, fill = cor)) +
   geom_tile() +
   facet_wrap(~ TF, nrow = 5) +
   # scale_fill_few() +
+  theme_few() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+ggsave(file.path('..', 'Plots', 'cor-by-tf.pdf'))
+
+
+#
+# Summarise binding across all cells for each TF
+#
+summarise.binding <- function(tf) {
+  #
+  # Load the binding data
+  #
+  chip <- load.chip.features(tf)
+  #
+  # Summarise binding across all cell types
+  #
+  Reduce(function(x, y) x | 'B' == y, chip, Rle(FALSE, nrow(chip)))
+}
+tf.binding <- lapply(tf.levels, summarise.binding)
+names(tf.binding) <- tf.levels
+tf.pairs <- combn(tf.levels, 2)
+pair.cors <- sapply(1:ncol(tf.pairs), function(i) pearson.cor(tf.binding[[tf.pairs[1,i]]], tf.binding[[tf.pairs[2,i]]]))
+tf.binding.cors <- data.frame(
+  tf.1 = factor(tf.pairs[1,], levels = tf.levels),
+  tf.2 = factor(tf.pairs[2,], levels = tf.levels),
+  cor = pair.cors)
+# devtools::use_data(tf.binding.cors)
+
+#
+# Make a plot of between-TF binding correlations
+#
+ggplot(tf.binding.cors, aes(x = tf.1, y = tf.2, fill = cor)) +
+  geom_tile() +
   theme_few() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 ggsave(file.path('..', 'Plots', 'cor-by-tf.pdf'))
