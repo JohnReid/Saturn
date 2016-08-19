c19e7b1a9db4d797bcf6beef759e1e4f6c9b8b9f
JohnReid <johnbaronreid@netscape.net>
2016-08-19 11:30:39 +0100

    Add footprinting to scan feature script.

--- scripts/feature-scan.R
+++ scripts/feature-scan.R
@@ -2,7 +2,7 @@
 #
 # Make features from motif scan
 #
-"Usage: feature-scan.R SCANDIR SCANTAG" -> doc
+"Usage: feature-scan.R [--tf=TF] [--cell=CELL]... [--wellington] SCANDIR SCANTAG" -> doc
 
 
 #
@@ -21,23 +21,28 @@ library(Saturn)
 #
 # Parse options
 #
-# .args <- "/home/john/Dev/DREAM-ENCODE/Data/Motifs/Known/ TestScan"
+.args <- "--cell=A549 --wellington /home/john/Dev/DREAM-ENCODE/Data/Motifs/Known/ TestScan"
 if (! exists(".args")) .args <- commandArgs(TRUE)
 opts <- docopt::docopt(doc, args = .args)
 print(opts)
 scan.dir <- opts[['SCANDIR']]
 scan.tag <- opts[['SCANTAG']]
+cells <- opts$cell
+tf <- opts$tf
+wellington <- opts$wellington
 
 
 #
 # Set up
 #
-features.file.name <- feature.file.name(scan.tag)
 stopifnot(file.exists(scan.dir))
-stopifnot(file.exists(dirname(features.file.name)))
+stopifnot(file.exists(file.path(features.dir, scan.tag)))
 if (file.exists(features.file.name)) {
   message('Features already exist, stopping: ', features.file.name)
   quit(save = 'no')
+}
+if (wellington && ! length(cells)) {
+  stop('Wellington: no cells specified.')
 }
 
 
@@ -47,9 +52,8 @@ if (file.exists(features.file.name)) {
 .scan <- load.motif.dir(scan.dir)
 
 
-#
-# Generate feature for each motif
-#
+#' Generate feature for each motif
+#'
 calc.feature <- function(hits.gr) {
   overlaps <- as.data.frame(findOverlaps(ranges.test(), hits.gr, ignore.strand = TRUE))
   overlaps$value <- mcols(hits.gr[overlaps$subjectHits])$logBF
@@ -57,11 +61,58 @@ calc.feature <- function(hits.gr) {
     aggregate(overlaps %>% select(-subjectHits), list(overlaps$queryHits), max),
     Rle.from.sparse(length(ranges.test()), queryHits, value))
 }
+
+
+#' Subset ranges by footprints
+#'
+subsetByFootprints <- function(gr, cell) {
+  message('Subsetting by footprints: ', cell)
+  footprints <- load.wellington(cell)
+  subsetByOverlaps(gr, footprints)
+}
+
+
+#' Save features
+#'
+save.features <- function(features, features.file.name) {
+  message('Saving features: ', features.file.name)
+  saveRDS(do.call(S4Vectors::DataFrame, features), features.file.name)
+}
+
+
+#
+# Are we subsetting by Wellington footprints?
+#
+if (wellington) {
+  for (cell in cells) {
+    message('Calculating features in: ', cell)
+    #
+    # If a TF is specified name the feature file with it
+    #
+    if (is.null(tf)) {
+      features.file.name <- feature.cell.file.name(scan.tag, cell)
+    } else {
+      features.file.name <- feature.tf.cell.file.name(scan.tag, tf, cell)
+    }
+    #
+    # Calculate and save features
+    features <-
+      lapply(
+        lapply(.scan, functional::Curry(subsetByFootprints, cell = cell)),
+        calc.feature)
+    save.features(features, features.file.name)
+  }
+}
+
+#
+# If a TF is specified name the feature file with it
+#
+if (is.null(tf)) {
+  features.file.name <- feature.file.name(scan.tag)
+} else {
+  features.file.name <- feature.tf.file.name(scan.tag, tf)
+}
+#
+# Calculate and save features
 features <- lapply(.scan, calc.feature)
-
-
-#
-# Save features
-#
-message('Saving features: ', features.file.name)
-saveRDS(features, features.file.name)
+save.features(features, features.file.name)
