47bf3e46f53b1ff5fe5a54356c92ac4f5c863ef4
JohnReid <johnbaronreid@netscape.net>
2016-08-21 15:05:26 +0100

    Add SBATCH directives

--- scripts/feature-scan.R
+++ scripts/feature-scan.R
@@ -2,6 +2,20 @@
 #
 # Make features from motif scan
 #
+#!
+#! sbatch directives begin here ###############################
+#!
+#SBATCH -p mrc-bsu-sand                 # Partition
+#SBATCH -J FEATSCAN                     # Name of the job
+#SBATCH -A MRC-BSU-SL2                  # Which project should be charged
+#SBATCH --nodes=1                       # How many whole nodes should be allocated?
+#SBATCH --ntasks=1                      # How many (MPI) tasks will there be in total? (<= nodes*16)
+#SBATCH --mem=15360                     # How many MB each node is allocated
+#SBATCH --time=06:00:00                 # How much wallclock time will be required?
+#SBATCH -o feat-scan-%j.out             # stdout
+#SBATCH -e feat-scan-%j.out             # stderr
+#SBATCH --mail-type=FAIL                # What types of email messages do you wish to receive?
+##SBATCH --no-requeue                   # Uncomment this to prevent the job from being requeued (e.g. if
 "Usage: feature-scan.R [--tf=TF] [--cell=CELL]... [--wellington] SCANDIR SCANTAG" -> doc
 
 
@@ -36,8 +50,8 @@ wellington <- opts$wellington
 #
 # Set up
 #
+message('Scan directory: ', scan.dir)
 stopifnot(file.exists(scan.dir))
-stopifnot(file.exists(file.path(features.dir(), scan.tag)))
 if (wellington && ! length(cells)) {
   stop('No cells specified for Wellington footprints.')
 }
